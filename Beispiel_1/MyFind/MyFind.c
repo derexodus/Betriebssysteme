76265b80042898e88ed6e7b289286d93f837170c
Steiner Louis Daniel <louis.steiner@ecosio.com>
2017-02-28 20:20:30 +0100

    ADD -print Function for directory Beispiel_1/MyFind

--- Beispiel_1/MyFind/MyFind.c
+++ Beispiel_1/MyFind/MyFind.c
@@ -4,35 +4,71 @@
 #include <dirent.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
 
-void do_dir(const char *dir_name, const char *const *parms)
+void strupr(char *sPtr)
+{
+  while(*sPtr != '\0')
+    *sPtr = toupper(*sPtr);
+}
+
+int GoThrough(const char *source, const int level)
 {
     DIR *dir;
     struct dirent *entry;
 
-    if(!(dir = opendir(dir_name)))
-        return;
+    if(!(dir = opendir(source)))
+        return -1;
     if(!(entry = readdir(dir)))
-        return;
+        return -2;
+
+    if(level == 0)
+        printf("%s\n",source);
 
     do
     {
-        if(entry->d_type == 4 && (strcmp(entry->d_name, "..") == 0))
-            continue;
         if(entry->d_type == 4 && (strcmp(entry->d_name, ".") == 0))
             continue;
+
+        if(entry->d_type == 4 && (strcmp(entry->d_name, "..") == 0))
+            continue;
+
+
         if(entry->d_type == 4 && !(strcmp(entry->d_name, ".") == 0))
         {
-             char *result = malloc(strlen(dir_name)+strlen(entry->d_name)+2);
-             strcpy(result, dir_name);
-             strcat(result, "/");
-             strcat(result,entry->d_name);
-             do_dir(result,NULL);
+            char *result = malloc(strlen(source)+strlen(entry->d_name)+2);
+            strcpy(result, source);
+            strcat(result, "/");
+            strcat(result,entry->d_name);
+            GoThrough(result, level + 1);
         }
-       // printf("%d\n", entry->d_type);
-        printf("%2d %5s %s/%s  %d\n", entry->d_ino, dir_name, entry->d_name);
-    }while(entry = readdir(dir));
+        printf("%s/%s\n", source, entry->d_name);
+    }while((entry = readdir(dir)) != NULL);
+
     closedir(dir);
+    return 0;
+}
+
+int do_dir(const char * dir_name, const char * const * parms)
+{
+    int i = 2;
+
+  //  printf("%s   %s \n",dir_name, parms[1]);
+
+    while(parms[i] != NULL && strcmp(parms[i],"(null)") != 0)
+    {
+        if(strcmp(parms[i],"-print") == 0)
+        {
+            GoThrough(dir_name,0);
+        }
+        else
+        {
+            printf("Unknown parameter!\n");
+        }
+        i++;
+    }
+
+    return 0;
 }
 
 
@@ -40,7 +76,17 @@ int main(int argc, char *argv[])
 {
     if(argc == 1)
     {
-        do_dir(".", NULL);
+        GoThrough(".",0);
+        printf("%s",argv[0]);
+    }
+    else if(argc == 2)
+    {
+        GoThrough(argv[1],0);
+        printf("%s",argv[0]);
+    }
+    else if(argc > 2)
+    {
+        do_dir(argv[1], (const char**)argv);
     }
 
     return 0;
